484d00cd10a43457d887d8050e1ded3485fd35d0
Gangadhar-Raj <Gangadharraj25@gmail.com>
2023-05-25 19:14:02 +0530

    Architect asian develop

---
+++ src/main/java/MY/Tmoney/TestingGitHub.java
@@ -0,0 +1,14 @@
+package MY.Tmoney;
+
+import org.testng.annotations.Test;
+
+public class TestingGitHub {
+
+	
+	@Test
+	public void testBranch()
+	{
+		  System.out.println("Hello GitXcreating branch ");
+
+	}
+}
