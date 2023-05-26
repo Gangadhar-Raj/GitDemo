9e7d55632355dc14693ec75eb51e6713bf0f12f1
Gangadhar-Raj <Gangadharraj25@gmail.com>
2023-05-26 18:37:23 +0530

    Final commit by asian devep team

---
+++ src/main/java/MY/Tmoney/TestMerge.java
@@ -0,0 +1,15 @@
+package MY.Tmoney;
+
+import org.testng.annotations.Test;
+
+public class TestMerge {
+	
+  
+	
+	@Test
+	public void testingmer()
+	{
+		System.out.println("Hello Final");
+	}
+
+}
